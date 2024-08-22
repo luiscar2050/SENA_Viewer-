4d69798170099a4825da1a8a66c4b7e9d3469614
luiscar2050 <luis3206162549@gmail.com>
2024-08-22 08:20:41 -0600

    adelanto menu en main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import edu.misena.senaviewer.model.Chapter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion;
+
+        do{
+            String message = "\n\nMenu SenaViewed\n\n";
+
+            message += "1. Book\n";
+            message += "2. Chapter\n";
+            message += "3. Movie\n";
+            message += "4. Serie\n";
+            message += "5. Magazine\n";
+            message += "6. Salir...\n\n";
+
+            System.out.println(message);
+
+            System.out.println("Elige tu Menu: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion){
+                case 1:
+                    break;
+                case 2:
+                    Chapter.menuChapter();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Ingrese una opción valida");
+                    break;
+
+            }
+
+
+        }while(opcion !=6);
     }
 }
