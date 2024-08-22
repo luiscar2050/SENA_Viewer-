4ea2f563a4f8f92d460ed9f1d9a1f56855a5b999
luiscar2050 <luis3206162549@gmail.com>
2024-08-22 08:03:04 -0600

    se agrega a la clase Magazine el constructor con los getters y setters para acceder a los atributos

--- src/edu/misena/senaviewer/model/Magazine.java
+++ src/edu/misena/senaviewer/model/Magazine.java
@@ -1,4 +1,58 @@
 package edu.misena.senaviewer.model;
 
+import java.util.Date;
+
 public class Magazine {
+
+    int id;
+    String title;
+    Date editionDate;
+    String editorial;
+    String[] authors;
+
+    public Magazine(String title,Date edititionDate,String editorial){
+        this.title = title;
+        this.editionDate = edititionDate;
+        this.editorial = editorial;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getEditionDate() {
+        return editionDate;
+    }
+
+    public void setEditionDate(Date editionDate) {
+        this.editionDate = editionDate;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
 }
